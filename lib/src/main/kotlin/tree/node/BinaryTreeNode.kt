523f8ba8462782aef4d4d13d4d5429f1c647061f
Demon32123 <dmitry.sheiko@mail.ru>
2024-03-23 19:53:11 +0300

    feat: created an abstract node class

--- lib/src/main/kotlin/tree/node/BinaryTreeNode.kt
+++ lib/src/main/kotlin/tree/node/BinaryTreeNode.kt
@@ -1,4 +1,8 @@
 package tree.node
 
-internal abstract class BinaryTreeNode {
-}
+abstract class BinaryTreeNode<K: Comparable<K>, V, Node>(
+    val key: K,
+    var value: V,
+    var right: Node? = null,
+    var left: Node? = null
+)
