cfdd4cee6ffaabd31910b31cb7b89bfdf6c08b59
Demon32123 <dmitry.sheiko@mail.ru>
2024-03-25 00:02:44 +0300

    feat: сreated a AVL-Tree class

--- lib/src/main/kotlin/tree/AVLTree.kt
+++ lib/src/main/kotlin/tree/AVLTree.kt
@@ -1,4 +1,17 @@
 package tree
 
-class AVLTree {
+import tree.node.AVLTreeNode
+
+class AVLTree<K : Comparable<K>, V> : SearchTree<K, V, AVLTreeNode<K, V>>() {
+    override fun insertNode(node: AVLTreeNode<K, V>) {
+        TODO("Insert node to tree")
+    }
+
+    override fun removeNode(node: AVLTreeNode<K, V>) {
+        TODO("Remove node in tree")
+    }
+
+    override fun createNode(key: K, value: V): AVLTreeNode<K, V> {
+        TODO("Create a node")
+    }
 }
