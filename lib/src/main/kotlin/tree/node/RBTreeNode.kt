74712774f0b2c70f6a99566de9ee8bf6c12e50be
Demon32123 <dmitry.sheiko@mail.ru>
2024-03-25 00:32:58 +0300

    feat: сreated class red black tree node

--- lib/src/main/kotlin/tree/node/RBTreeNode.kt
+++ lib/src/main/kotlin/tree/node/RBTreeNode.kt
@@ -1,4 +1,12 @@
 package tree.node
 
-class RBTreeNode {
+enum class RBTreeColor {
+    BLACK, RED
 }
+
+class RBTreeNode<K : Comparable<K>, V>(
+    key: K,
+    value: V,
+    var color: RBTreeColor = RBTreeColor.RED,
+    var parent: RBTreeNode<K, V>? = null
+) : BinaryTreeNode<K, V, RBTreeNode<K, V>>(key, value)
