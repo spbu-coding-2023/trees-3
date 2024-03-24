4df5e1dfb4e94f606e48348255e0cd7a455d87de
Demon32123 <dmitry.sheiko@mail.ru>
2024-03-25 00:37:22 +0300

    feat: сreated class red black tree

--- lib/src/main/kotlin/tree/RBTree.kt
+++ lib/src/main/kotlin/tree/RBTree.kt
@@ -1,5 +1,17 @@
 package tree
 
-class RBTree {
+import tree.node.RBTreeNode
 
+class RBTree<K : Comparable<K>, V> : SearchTree<K, V, RBTreeNode<K, V>>() {
+    override fun insertNode(node: RBTreeNode<K, V>) {
+        TODO("Insert node to tree")
+    }
+
+    override fun removeNode(node: RBTreeNode<K, V>) {
+        TODO("Remove node in tree")
+    }
+
+    override fun createNode(key: K, value: V): RBTreeNode<K, V> {
+        TODO("Create a node")
+    }
 }
