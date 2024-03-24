50e2689114063bb1118254de5873f904e36a30e1
Demon32123 <dmitry.sheiko@mail.ru>
2024-03-24 23:54:24 +0300

    feat: сreated a binary search tree class

--- lib/src/main/kotlin/tree/BSTree.kt
+++ lib/src/main/kotlin/tree/BSTree.kt
@@ -1,4 +1,17 @@
 package tree
 
-class BSTree {
+import tree.node.BSTreeNode
+
+class BSTree<K : Comparable<K>, V> : SearchTree<K, V, BSTreeNode<K, V>>() {
+    override fun insertNode(node: BSTreeNode<K, V>) {
+        TODO("Insert node to tree")
+    }
+
+    override fun removeNode(node: BSTreeNode<K, V>) {
+        TODO("Remove node in tree")
+    }
+
+    override fun createNode(key: K, value: V): BSTreeNode<K, V> {
+        TODO("Create a node")
+    }
 }
