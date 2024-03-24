1ebd229f3ede9d333ff93fb6938d1a4e40679c48
Demon32123 <dmitry.sheiko@mail.ru>
2024-03-24 17:57:48 +0300

    fix: the order of the AVL-Tree node class attributes has been changed

--- lib/src/main/kotlin/tree/node/AVLTreeNode.kt
+++ lib/src/main/kotlin/tree/node/AVLTreeNode.kt
@@ -1,4 +1,4 @@
 package tree.node
 
-internal class AVLTreeNode<K : Comparable<K>, V>(var height: Int = 1, key: K, value: V) :
+internal class AVLTreeNode<K : Comparable<K>, V>(key: K, value: V, var height: Int = 1) :
     BinaryTreeNode<K, V, BSTreeNode<K, V>>(key, value)
