ddd5d3f7c654592a65cdb97e9e54f8275928b3da
Ilhom Kombaev <kombaev02@gmail.com>
2024-03-27 20:09:33 +0300

    feat(RBT): implement rotation methods

--- lib/src/main/kotlin/tree/RBTree.kt
+++ lib/src/main/kotlin/tree/RBTree.kt
@@ -14,4 +14,48 @@ class RBTree<K : Comparable<K>, V> : SearchTree<K, V, RBTreeNode<K, V>>() {
     override fun createNode(key: K, value: V): RBTreeNode<K, V> {
         return RBTreeNode(key, value)
     }
+
+    private fun leftRotation(x: RBTreeNode<K, V>): Boolean {
+        val y = x.right ?: return false
+
+        x.right = y.left
+        y.left?.parent = x;
+        y.parent = x.parent
+
+        val parent = x.parent
+        if (parent == null) {
+            root = y
+        } else if (parent.left == x) {
+            parent.left = y
+        } else {
+            parent.right = y
+        }
+
+        y.left = x
+        x.parent = y
+
+        return true
+    }
+
+    private fun rightRotation(y: RBTreeNode<K, V>): Boolean {
+        val x = y.left ?: return false
+
+        y.left = x.right
+        x.right?.parent = y
+        x.parent = y.parent
+
+        val parent = y.parent
+        if (parent == null) {
+            root = x
+        } else if (parent.left == y) {
+            parent.left = x
+        } else {
+            parent.right = x
+        }
+
+        x.right = y
+        y.parent = x
+
+        return true
+    }
 }
