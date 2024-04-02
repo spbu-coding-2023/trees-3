48d8a531c36a37c80b76cd19ecc10a9ef2be8994
Ilhom Kombaev <kombaev02@gmail.com>
2024-04-03 02:14:45 +0300

    test(RBT): add constructors tests

--- lib/src/test/kotlin/tree/RBTreeTest.kt
+++ lib/src/test/kotlin/tree/RBTreeTest.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
-import tree.node.RBTreeNode
 
 class RBTreeTest {
     private lateinit var rbt: RBTree<Int, String>
@@ -13,6 +12,43 @@ class RBTreeTest {
     @BeforeEach
     fun setup() {
         rbt = RBTree()
+    }
+
+    @Nested
+    inner class `Constructor tests` {
+        @Test
+        fun `without args`() {
+            assertEquals(0, RBTree<Int, String>().size)
+            assertEquals(0, RBTree<Int, String>().getEntities().size)
+        }
+
+        @Test
+        fun `one entity`() {
+            val rbt = RBTree(1, "Homka")
+            assertEquals("Homka", rbt.search(1))
+        }
+
+        @Test
+        fun `array of entities`() {
+            val entities = arrayOf(
+                Pair(35, 1),
+                Pair(21, 1),
+                Pair(25, 1),
+                Pair(62, 1),
+                Pair(12, 1),
+                Pair(62, 1),
+                Pair(122, 1),
+                Pair(621, 1),
+                Pair(121, 1),
+                Pair(362, 1),
+                Pair(35, 1),
+                Pair(523, 1),
+            )
+
+            val rbt = RBTree(entities)
+            assertEquals(10, rbt.size)
+            assertEquals(listOf(12, 21, 25, 35, 62, 121, 122, 362, 523, 621), rbt.getKeys())
+        }
     }
 
     @Nested
