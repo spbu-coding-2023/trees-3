44f77fdc33a7d1e3ceeecb00a5d21b264ec85735
Anastasiia Kuzmina <anastasiia.kuzmina7@gmail.com>
2024-08-29 01:08:58 +0300

    feat(Treap): add private method generatePrior

--- lib/src/main/kotlin/tree/Treap.kt
+++ lib/src/main/kotlin/tree/Treap.kt
@@ -1,11 +1,26 @@
 package tree
 
 import tree.node.TreapNode
+import kotlin.random.Random
 
 class Treap <K : Comparable<K>, V> : SearchTree<K, V, TreapNode<K, V>> {
     constructor() : super()
     constructor(key: K, value: V) : super(key, value)
     constructor(pairs: Array<Pair<K, V>>) : super(pairs)
+
+    private var allPriors = mutableListOf(0)
+
+    private fun generatePrior(node: TreapNode<K, V>){
+
+        if (node.prior == 0){
+            node.prior = Random.nextInt()
+        }
+        if (allPriors.contains(node.prior)){
+            generatePrior(node)
+        } else {
+            allPriors.add(node.prior)
+        }
+    }
 
     override fun insertNode(node: TreapNode<K, V>) {}
 
