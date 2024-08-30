e87d5e2f23e02f87f4f7089ff19ba50d3e68f675
Anastasiia Kuzmina <anastasiia.kuzmina7@gmail.com>
2024-08-30 18:27:41 +0300

    feat(Treap): add file TreapTest

---
+++ lib/src/test/kotlin/tree/TreapTest.kt
@@ -0,0 +1,12 @@
+package tree
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class TreapTest{
+    private lateinit var treap: Treap<Int, String>
+    private val treapEmpty = Treap<Int, String>()
+
+}
